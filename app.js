2d03ab214dcefa1f6cf81d491e8116d6c9e108a4
Divya <divyaa.tagline@gmail.com>
2020-11-11 17:47:46 +0530

    default rtoute for testing set

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/', (req, res) => {
+  res.send('App is running');
+})
 app.use('/student', studentRouter);
 app.use('/teacher', teacherRouter);
 
